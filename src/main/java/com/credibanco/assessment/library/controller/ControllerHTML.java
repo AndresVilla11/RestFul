edb525c37fcc81d4dd831e9b8ffe65f6f5abd55d
AndresVilla11 <a.villa11@hotmail.com>
2021-02-24 12:34:49 -0500

    front-end

--- src/main/java/com/credibanco/assessment/library/controller/ControllerHTML.java
+++ src/main/java/com/credibanco/assessment/library/controller/ControllerHTML.java
@@ -5,15 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = "/registrar")
 public class ControllerHTML {
 
-	@RequestMapping(value = { "/", "/Home" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/libro" }, method = RequestMethod.GET)
 	public String controlDeInicio() {
 		return "Home.html";
 	}
 	@RequestMapping(value = { "creacion/libro" }, method = RequestMethod.GET)
 	public String control() {
-		return "redirect:/Home";
+		return "redirect:/registrar/libro";
 	}
 	@RequestMapping(value = { "error" }, method = RequestMethod.GET)
 	public String error() {
